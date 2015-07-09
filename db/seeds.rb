2682de4c5eb537dbf02fb5fb2c92085fdd76bfe2
rails-udhaya <udhaya.dhanabalan@gmail.com>
2015-07-08 18:22:02 -0700

    added twitire data

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,15 @@ lis_1.each do |l_1|
                 create_atdonline_lists = AtdonlineList.create(:category => "tires",:subcategory => "passenger_and_light_truck", :brand_name => l_1, :is_enabled => true)
         end
 end	
+
+
+lis_2=[["Antares","http://new.tirelibrary.com/images/Manflogos/antares.png"],["Carlisle","http://new.tirelibrary.com/images/Manflogos/CARLISLE.png"],["Doublecoin","http://new.tirelibrary.com/images/Manflogos/DOUBLECOIN.png"],["Duraturn","http://new.tirelibrary.com/images/Manflogos/Duraturn.png"],["Durun","http://new.tirelibrary.com/images/Manflogos/DURUN.png"],["Federal","http://new.tirelibrary.com/images/Manflogos/FEDERAL.png"],["GTRadial","http://new.tirelibrary.com/images/Manflogos/GTRADIAL.png"],["Hankook","http://new.tirelibrary.com/images/Manflogos/HANKOOK.png"],["Hoosier","http://new.tirelibrary.com/images/Manflogos/HOOSIER.png"],["Nexen","http://new.tirelibrary.com/images/Manflogos/NEXEN.png"],["Prometer","http://new.tirelibrary.com/images/Manflogos/PROMETER.png"],["Radar","http://new.tirelibrary.com/images/Manflogos/RADAR.png"],["Sentinel","http://new.tirelibrary.com/images/Manflogos/sentinel.png"],["Starfire","http://new.tirelibrary.com/images/Manflogos/STARFIRE.png"],["Venezia","http://new.tirelibrary.com/images/Manflogos/Venezia.png"],["Yokohama","http://new.tirelibrary.com/images/Manflogos/YOKOHAMA.png"]]
+lis_2.each do |l|
+category=""		
+image_url=""		
+category=l[0]
+image_url =l[1]
+		     if !TwitireList.exists?(:category => category)
+                create_twitire_lists = TwitireList.create(:category => "#{category}",:image_url => "#{image_url}", :is_enabled => true)
+        end
+end
