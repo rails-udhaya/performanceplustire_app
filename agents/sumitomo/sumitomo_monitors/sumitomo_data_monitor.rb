48b9b458d5d74669e4756547e7b053f8116d5981
rails-udhaya <udhaya.dhanabalan@gmail.com>
2015-09-02 15:41:44 -0700

    partially added sumito functionality

--- agents/sumitomo/sumitomo_monitors/sumitomo_data_monitor.rb
+++ agents/sumitomo/sumitomo_monitors/sumitomo_data_monitor.rb
@@ -3,18 +3,18 @@ require 'logger'
 
 
 						Dir.mkdir("#{File.dirname(__FILE__)}/logs") unless File.directory?("#{File.dirname(__FILE__)}/logs")
-						$logger = Logger.new("#{File.dirname(__FILE__)}/logs/kumho_data_monitor.log", 'weekly')
+						$logger = Logger.new("#{File.dirname(__FILE__)}/logs/sumitomo_data_monitor.log", 'weekly')
 						#~ $logger.level = Logger::DEBUG
 						$logger.formatter = Logger::Formatter.new
 
 #~ while true do
-pid_status_english = system("ps -aux | grep kumho_data_agent.rb | grep -vq grep")
+pid_status_english = system("ps -aux | grep sumitomo_data_agent.rb | grep -vq grep")
 				if pid_status_english
 						$logger.info ("nothing to do....")
 				else
 						$logger.info ("Process started....")
-						#~ system("nohup bundle exec /usr/bin/ruby ../kumho_data_agent.rb &")
-						system("nohup bundle exec /usr/bin/ruby /var/www/apps/performanceplustire/current/agents/kumho/kumho_data_agent.rb -e production &")
+						#~ system("nohup bundle exec /usr/bin/ruby ../sumitomo_data_agent.rb &")
+						system("nohup bundle exec /usr/bin/ruby /var/www/apps/performanceplustire/current/agents/sumitomo/sumitomo_data_agent.rb -e production &")
 				end
 #~ sleep 300
 #~ end
